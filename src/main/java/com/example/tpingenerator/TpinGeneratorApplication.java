a9d2b0d8b0acfd0a5a7e840628f2a75c70e6ce30
AshishB <bishtashish936@gmail.com>
2022-10-26 20:49:22 +0530

    initial commit

---
+++ src/main/java/com/example/tpingenerator/TpinGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpingenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpinGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpinGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tpingenerator/TpinGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpingenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpinGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
